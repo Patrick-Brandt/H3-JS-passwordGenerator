1282ecc4457f034f7c68b6562d1526bdd7ece63f
Patrick Brandt <brandt.patrickc@gmail.com>
2021-01-11 15:44:23 -0800

    I added some 'to-do' notes. Changed the vars I created to camelCase. Moved vars underneath the function 'generatePassword'. Now the 'confirms' are working when you press the 'generate password' button. Its starting to come together! For now, pressing button generates an 'undefined'.

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 // This is my TO-DO LIST
 
-// When you click the button, you are presented with a series of prompts.
-  // Create series of prompts 
+// When you click the button, you are presented with a series of prompts (confirms).
+  
 //When prompted for password criteria, you select what you want to include.
 
 //When you select what password criteria to include, you're prompted for the length of password (8 to 128).
-
+  //add a prompt, so user can add input (confirm is just a yes/no).
 //When prompted for character types, you can choose  lawercase, uppercase, numeric, and /or special characters.
-    // I think I need to add all lowercase, uppercase, numbers, and special characters as arrays?
+    
 //when you answer each prompt, your input should be validated.
-
+  //Add a question for if they put in incorrect InputDeviceInfo.
 //when all prompts are answered, a password is generated, THEN displayed in an alert or written on page.
+
+//Will need to add if/else statements somewhere, but I'm not sure where.
+//Conditional statement somewhere?
+
+//Will need to find a randomizing function. 'Math.random' 'math.floor' or something like that?
+    //generate a random password. Something like this. (Found on geeks for geeks.org) 
+    //for (int i =0; i<10; i++){ int rand= (int)(math.random() * range) +min;
+    // System.out.println(rand);}}   } 
 
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var special characters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "/", ":", ";", "<", ">", "," , "?", "."];
-
-var confirmlower = confirm ("Do you want to use lowercase letters in your password?");
-var confirmupper = confirm ("Do you want to use uppercase letters in your password?");
-var confirmnumbers = confirm ("Do you want to use numbers in your password?");
-var confirmspecial = confirm ("Do you want to use special characters in your password?");
+var specialCharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "/", ":", ";", "<", ">", "," , "?", "."];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -32,6 +35,13 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword(){
+  var confirmLower = confirm ("Do you want to use lowercase letters in your password?");
+  var confirmUpper = confirm ("Do you want to use uppercase letters in your password?");
+  var confirmNumbers = confirm ("Do you want to use numbers in your password?");
+  var confirmSpecial = confirm ("Do you want to use special characters in your password?");
 }
 
 // Add event listener to generate button
