4386b4ef7444222e40cff3e34822469182da96a9
Patrick Brandt <brandt.patrickc@gmail.com>
2021-01-11 16:38:28 -0800

    I added comments and more notes to myself.

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // This is my TO-DO LIST
-
-// When you click the button, you are presented with a series of prompts (confirms).
-  
-//When prompted for password criteria, you select what you want to include.
-
-//When you select what password criteria to include, you're prompted for the length of password (8 to 128).
-  //add a prompt, so user can add input (confirm is just a yes/no).
-//When prompted for character types, you can choose  lawercase, uppercase, numeric, and /or special characters.
     
 //when you answer each prompt, your input should be validated.
-  //Add a question for if they put in incorrect InputDeviceInfo.
+  //Add a question for if they put in incorrect info.
+
 //when all prompts are answered, a password is generated, THEN displayed in an alert or written on page.
+
+//I need to get the questions to actually return information, but don't remember how. 'Return.uppercase' or something like that?
 
 //Will need to add if/else statements somewhere, but I'm not sure where.
 //Conditional statement somewhere?
+
+//will need to use 'concat' somewhere once they have chosen character types. I think you create a new var,
+// and then combime the strings in it.
+    // ex: var combo = lowercase.concat(uppercase,numbers);  .Will look something like this.
 
 //Will need to find a randomizing function. 'Math.random' 'math.floor' or something like that?
     //generate a random password. Something like this. (Found on geeks for geeks.org) 
     //for (int i =0; i<10; i++){ int rand= (int)(math.random() * range) +min;
     // System.out.println(rand);}}   } 
 
+//These arrays contain all of the characters that can possibly be used in the password.
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -37,6 +37,7 @@ function writePassword() {
 
 }
 
+//This is the function that contains the prompts that show up on screen.
 function generatePassword(){
   var confirmLower = confirm ("Do you want to use lowercase letters in your password?");
   var confirmUpper = confirm ("Do you want to use uppercase letters in your password?");
