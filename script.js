fe46361a1aed31f282b3c90396efa5164e647f4b
Patrick Brandt <brandt.patrickc@gmail.com>
2021-01-14 18:47:34 -0800

    removed quotations from lines 20-23. they weren't necessary. Edited the for-loop on line 88. added a console.log underneath to check to see if code worked, returned password and boom! It works

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "/", ":", ";", "<", ">", "," , "?", "."];
-var chosenCharacters = [""];
-var confirmLength = [""];
-var generatedPassword = [""]
-var passwordText = [""]
-var password = [""]
+var chosenCharacters = [];
+var confirmLength = [];
+var generatedPassword = [];
+var passwordText = [];
+var password = "";
 //This is the minimum and maximum length that the user can choose for their password.
 var passwordMax = 128;
 var passwordMin = 8;
@@ -62,7 +62,7 @@ function generatePassword(){
     chosenCharacters += numbers;
     //I'm using a console.log for each confirm var to see if my code is actually storing the info in the chosenCharacters var.
     console.log (chosenCharacters)
-}
+  }
 
   var confirmSpecial = confirm ("Do you want to use special characters in your password?");
   //The 'if' statements log the user's choice to the chosenCharacters var.
@@ -70,27 +70,30 @@ function generatePassword(){
     chosenCharacters += specialCharacters;
     //I'm using a console.log for each confirm var to see if my code is actually storing the info in the chosenCharacters var.
     console.log (chosenCharacters)
-}
+  }
 
   //If statement in case the user doesn't choose any characters.
   if (!confirmLower && !confirmUpper && !confirmNumbers && !confirmSpecial) {
-  alert("You must choose at least one of the character options! Try again.");
-  } else {
-  var confirmLength = prompt ("How many characters do you want in your password? Please choose a number between 8 and 128.");
-  console.log (confirmLength)
+    alert("You must choose at least one of the character options! Try again.");
+  } 
+  else {
+    var confirmLength = prompt ("How many characters do you want in your password? Please choose a number between 8 and 128.");
+    console.log (confirmLength)
   
-  //If user selects a number that is less than the minimum or more than the maximum, this alert will diplay.
-  if (confirmLength<passwordMin || confirmLength>passwordMax) {
-    alert("Password is not between 8 and 128 characters! Try again.");
-} else {
-  //This is where I need to write the code to that calculates the password and then returns that variable that contains the password.
-  for (var i=0; i<confirmLength; i++){
-    Math.floor(Math.random() * chosenCharacters.length);
-    passwordText.value += chosenCharacters[total];
-}
-return generatedPassword; 
-} 
-}     
+    //If user selects a number that is less than the minimum or more than the maximum, this alert will diplay.
+    if (confirmLength<passwordMin || confirmLength>passwordMax) {
+      alert("Password is not between 8 and 128 characters! Try again.");
+    } else {
+      //This is where I need to write the code to that calculates the password and then returns that variable that contains the password.
+      for (var i=0; i<confirmLength; i++){
+        var randomIndex = Math.floor(Math.random() * chosenCharacters.length);
+        password += chosenCharacters[randomIndex];
+      }
+      console.log(password) 
+      return password
+    } 
+    
+  }     
 }
 
 //for (var i = 0; i < confirmLength; i++) {
